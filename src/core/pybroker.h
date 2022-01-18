9a84592fef23ccf42970facafe310273165c0b57
hallestar <wongzicai@gmail.com>
2022-01-18 09:42:08 +0800

    修改embbed用例

--- src/core/pybroker.h
+++ src/core/pybroker.h
@@ -2,7 +2,7 @@
 #define TRYPYBIND_PYBROKER_H
 
 
-#if(defined WIN32|| defined _WIN32) && defined EFIMPL
+#if(defined WIN32|| defined _WIN32) && defined LDIMPL
 #define EXPORT __declspec(dllexport)
 #else
 #define EXPORT
